d8e2674f15840b98fcdce17de28d5251d728bd98
Rogerio Melo <rogerio@inovebr.com>
2021-06-18 16:43:07 -0300

    Add project files.

---
+++ Aula 5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Aula_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Acao;
+            string caminho = "usuario. txt";
+            string nome = "", email = "", Telefone = "", RG = "";
+
+            Console.WriteLine("####################");
+            Console.WriteLine("####################");
+            Console.WriteLine("G - GRAVAR----------");
+            Console.WriteLine("L - LER-------------");
+            Console.WriteLine("S - SAIR------------");
+            Console.WriteLine("####################");
+            Console.WriteLine("####################");
+            Console.WriteLine("Digite a operação:");
+
+            Acao = (Console.ReadLine().ToUpper());
+            {
+                Console.WriteLine();
+
+                while (Acao != "s")
+                {
+
+                    if (Acao == "G")
+                    {
+
+                        Console.WriteLine("Informe seu nome");
+
+                        nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe seu email");
+
+                        email = Console.ReadLine();
+
+                        Console.WriteLine("Informe seu telefone");
+
+                        Telefone = Console.ReadLine();
+
+                        Console.WriteLine("Informe seu RG");
+
+                        RG = Console.ReadLine();
+
+                        StreamWriter sw = new StreamWriter(caminho, true);
+
+                    }
+
+
+
+
+
+                }
+            }
+
+        }
+    }
+}
